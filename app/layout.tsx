1be9b583213bf4e0a4bbdfb7f24ccc2b298d9aef
leodots <2212.leonardo@gmail.com>
2025-07-30 16:44:29 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Leo's Blog",
+  title: "Leo.",
   description: "Leo's personal blog",
   icons: {
     icon: "/favicon.svg",
