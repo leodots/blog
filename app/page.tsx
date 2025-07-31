c8e6cf713700a0ab065924ae6dfeebcb733ecdfe
Leonardo Torres <2212.leonardo@gmail.com>
2025-07-31 17:50:52 -0300

    Creating model viewer for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import SectionContainer from "@/components/SectionContainer";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <SectionContainer>
+        <Hero />
+      </SectionContainer>
     </div>
   );
 }
