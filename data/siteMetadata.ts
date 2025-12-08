84ad76653ede88c3d534aeada0f86ebc9d79307b
Leonardo Torres <2212.leonardo@gmail.com>
2025-12-08 02:35:23 -0300

    Implementing About page.

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -1,3 +1,6 @@
 export const siteMetadata = {
-  email: 'itsleonardotorres@gmail.com',
-}
+  email: "itsleonardotorres@gmail.com",
+  github: "https://github.com/leodots",
+  linkedin: "https://linkedin.com/in/leodots",
+  instagram: "https://instagram.com/lleotorres",
+};
