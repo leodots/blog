fe80454fa3d23194cf40212c7deaa350fd2e040e
Leonardo Torres <2212.leonardo@gmail.com>
2025-12-10 18:02:19 -0300

    implementing animation when changing theme

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -11,11 +11,47 @@ const ThemeSwitcher = () => {
 
   useEffect(() => setMounted(true), [])
 
-  const toggleTheme = () => {
-    setTheme(resolvedTheme === "dark" ? "light" : "dark")
+  const toggleTheme = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const newTheme = resolvedTheme === "dark" ? "light" : "dark"
+
+    if (!document.startViewTransition) {
+      setTheme(newTheme)
+      return
+    }
+
+    const x = event.clientX
+    const y = event.clientY
+
+    const endRadius = Math.hypot(
+      Math.max(x, window.innerWidth - x),
+      Math.max(y, window.innerHeight - y)
+    )
+
+    const transition = document.startViewTransition(() => {
+      setTheme(newTheme)
+    });
+
+    transition.ready.then(() => {
+      const duration = 500
+      document.documentElement.animate(
+        {
+          clipPath: [
+            `circle(0px at ${x}px ${y}px)`,
+            `circle(${endRadius}px at ${x}px ${y}px)`,
+          ],
+        },
+        {
+          duration,
+          easing: "ease-in-out",
+          pseudoElement: "::view-transition-new(root)",
+        }
+      )
+    })
   }
 
-  if (!mounted) return <div className="ml-1 mr-1 h-8 w-8" />
+  if (!mounted) {
+    return <div className="ml-1 mr-1 h-8 w-8" />
+  }
 
   return (
     <motion.button
